c08014e6daf2947a278060885be15761a0e28f5c
Trần Hữu Lộc <loctran0169@gmail.com>
2019-12-02 20:21:50 +0700

    init projects

---
+++ QuanLyBenhVien/src/main/java/com/mycompany/main/frmGui.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.main;
+
+import com.mycompany.view.frmBenh;
+
+/**
+ *
+ * @author LQTPL
+ */
+public class frmGui {
+    public static void main(String[] args) {
+        new frmBenh().setVisible(true);
+    }
+}
